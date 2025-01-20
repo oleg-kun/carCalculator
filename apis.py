c8e5cdeaf4b4c618eef585ba70d16f31bed55ff8
Oleg Kun <oleg.gamayun@gmail.com>
2025-01-20 20:20:24 +0300

    Created new project

---
+++ apis.py
@@ -0,0 +1,10 @@
+def get_gas_price():
+    # TODO
+    return 8
+
+
+def get_power_price():
+    # TODO
+    return 1.2
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+import calculator
+
+
+if __name__ == '__main__':
+    calc = calculator.Calculator()
+    calc.add_car(
+        calculator.Car("Toyota Corolla", price=30000, fuel_economy=7, service_cost=1200, insurances_cost=2500)
+    )
+    calc.add_car(
+        calculator.ElectricCar("Tesla Model 3", 200000,fuel_economy=0, service_cost=0, insurances_cost=5500, power_consumption=150)
+    )
+    calc.add_car(
+        calculator.Car("Range Rover", 650000, 3, service_cost=3000, insurances_cost=7000)
+    )
+
+    calc.print_cars()
